9e9227f936b26366a81eb0546dd7349186ad2618
Your Name <you@example.com>
2016-12-11 22:34:18 -0800

    got the send_thanks completely working

--- src/mailroom.py
+++ src/mailroom.py
@@ -3,9 +3,13 @@ import io
 
 donors = []
 
-def Donor
+
+class Donor:
+    """Stores donor info."""
+
     first_name = ""
     last_name = ""
+    donations = []
     total_amount = 0
 
 
@@ -18,7 +22,7 @@ def initial_prompt():
         elif prompt == 'create report':
             create_report()
         elif prompt == 'send thank you':
-            donor_list()
+            send_thanks()
         else:
             print('Not a valid selection')
 
@@ -30,56 +34,33 @@ def quit_prompt():
         quit()
 
 
-def donor_dict(donor):
-    """Return the donor list info."""
-    donors = donor_list
-    dict = {}
-    # for donor in donors in enumerate():
-    #     if index % 3:
-    #         dict[item] = a[index + 1]
-    # return dict['first name', 'last name', 'total donations', 'average donation'] = 'donor[0]', 'donor[1]', 'donor[2]', 'donor[4]'
-    my_dict = {}
+def send_thanks():
+    donor = Donor()
+    donor.first_name = 'list'
+    while donor.first_name == 'list':
+        donor.first_name = input('Type first name of the donor OR type \'list\' to see a list of donors: ')
+        if donor.first_name == 'list':
+            name_list()
+    donor.last_name = input('Type the last name of the donor: ')
+    amount = 0
+    while amount == 0:
+        userInput = input('Enter donation amount: ')
+        try:
+            amount = int(userInput)
+        except ValueError:
+            print('Invalid input. Enter a number: ')
+    d = get_donor(donor)
+    d.donations.append(amount)
+    d.total_amount += amount
+    generate_email(d)
 
-
-# def create_dict(text):
-#     """Return {} with key value pairs."""
-#     first = text.split(' ')
-#     dic = {}
-#     for words in range(0, len(first) - 2):
-#         couple = "{0} {1}".format(first[words], first[words + 1])
-#         value = first[words + 2]
-#         dic.setdefault(couple, []).append(value)
-#     return dic
-
-
-def donor_list_prompt():
-    """Prompt user for a name or to view donor list."""
-    prompt = input('Type a donor name or \'donors\' to see a list of donors')
-    if prompt == 'donors':
-        name_list()
-    else:
-        update_donations(prompt)
-
-
-def update_donations(donors):
-    """Update donor values."""
-    new_donor = Donor()
-    new_donor.first_name = input('Please type first name of donor: ')
-    new_donor.last_name = input('Please type last name of donor: ')
-    new_donor.total_amount = input('Please type donation amount: ')
-
-    for donor in donors:
-        donor.append(donor_list)
-
-
-def name_list():
-    """Return a list of donors and print their names."""
-    donors = donor_list("return a list")
-    print('List of donors:')
-    for name in donors:
-        print(name[0])
-        donor_list_prompt()
-
+def get_donor(donor):
+    """Checks if inputted donor already exists in the system."""
+    for d in donors:
+        if d.first_name == donor.first_name and d.last_name == donor.last_name:
+            return d
+    donors.append(donor)
+    return donor
 
 def create_report():
     """Return and print ordered list of donor stats."""
@@ -90,45 +71,6 @@ def create_report():
         line = ('{0} has donated a total of ${1} in {2} donations at ${3} per''donation.'.format(donor[0], donor[1], donor[2], donor[3]))
         print(line)
         quit_prompt()
-
-
-def donor_calc(donations, value):
-    """Return donation values."""
-    donations[0] = int(donations[0]) + value
-    donations[1] = int(donations[1]) + 1
-    donations[2] = int(donations[2]) / int(donations[1])
-    return donations
-
-
-def sorted_list(first):
-    """Return sorted list of donors from biggest donations to smallest donations."""
-    pass
-
-
-def donation_prompt(person):
-    """Prompt and return an integer from user."""
-    value = input('How much did {0} donate? Type quit to quit'.format(person))
-    if value == 'quit':
-        quit()
-    else:
-        int(value)
-    return value
-
-
-def update_donations(person):
-    """Update donor values."""
-    donors = donor_list(read_donors(), 'dictionary')
-    value = int(donation_prompt(person))
-    if person in list(donors.keys()):
-        donations = donors[person]
-        new_donations = donor_calc(donations, value)
-        donors.setdefault(person, new_donations)
-    else:
-        donors.setdefault(person, [value, 1, value])
-    val = donors[person]
-    write_file(generate_text(donors))
-    generate_email(person, val[0])
-
 
 def generate_text(donors):
     """Return list of donors."""
@@ -141,33 +83,9 @@ def generate_text(donors):
         text_string = text_string + temp_val
         return text_string
 
-
-
-def write_file(text):
-    """Write donor list to .txt file."""
-    text_file = io.open('donors.txt', 'w')
-    text_file.write(text)
-    text_file.close()
-
-
-def generate_email(person, donations):
+def generate_email(donor):
     """Generate email for donor."""
-    print('\nWell met {0}!,\n We would like to thank you for your donation of ${1}.'.format(person, donations))
-    quit_prompt()
-
-
-def read_donors():
-    """Return donor txt file."""
-    openfile = io.open('donors.txt')
-    readfile = openfile.readlines()
-    openfile.close()
-    return readfile
-
-
-
-def donor_list(readfile, choice):
-    """Return list or dictionary depending on user choice."""
-
+    print('\nWell met {0} {1}!,\n We would like to thank you for your donation of ${2}.'.format(donor.first_name, donor.last_name, donor.donations[-1]))
 
 if __name__ == '__main__':
     initial_prompt()
